31545b3f7aa569703fc395ed978b7c032f3663ce
Zayaan <zayaanra@buffalo.edu>
2021-12-30 14:24:39 -0500

    Dependency for router

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,16 +1,23 @@
 /* This file includes all the things needed for the website as a whole. Each page will correspond to a separate component which will be included in 
 a separate folder. */
 
-import Home from './components/Home';
 import React from 'react';
 import { View } from "react-native";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from './components/Home';
 
 function App() {
   return (
-    <View>
-      <Home></Home>
-    </View>
+    <Router>
+      <View>
+      <Routes>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Routes>
+      </View>
+    </Router>
   );
 }
-
 export default App;
