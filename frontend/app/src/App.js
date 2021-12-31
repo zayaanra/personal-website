20966810416abd0108d9d9d117e04b50b4809ecf
Zayaan <zayaanra@buffalo.edu>
2021-12-31 00:38:10 -0500

    Added more routes

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -6,6 +6,10 @@ import { View } from "react-native";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from './components/Home';
+import Courses from './components/Courses';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Education from './components/Education';
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/home" element={<Home/>}/>
+        <Route path="/courses" element={<Courses/>}/>
+        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="/education" element={<Education/>}/>
       </Routes>
       </View>
     </Router>
