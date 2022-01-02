ec3d8cbee655c1764918e688785436e1d8c86bdd
Zayaan <zayaanra@buffalo.edu>
2022-01-01 20:17:51 -0500

    Added SVG anim.

--- frontend/app/src/components/Home.js
+++ frontend/app/src/components/Home.js
@@ -1,9 +1,9 @@
 /* This file will support the component for the homepage. It'll include a navbar, an image, and an About Me. */
 
-import { StyleSheet, View, Image, Text, Animated } from "react-native";
+import { StyleSheet, View, Image, Text, Animated, Dimensions } from "react-native";
 import React, { useRef } from 'react';
 
-import { motion } from "framer-motion";
+import { motion, AnimationProps } from "framer-motion";
 
 import TopBar from "./TopBar";
 import Links from "./Links";
@@ -62,13 +62,27 @@ const styles = StyleSheet.create({
     },
 })
 
-const svgV = {
-    hidden: { rotate: -180 },
-    visible: {
-        rotate: 0,
-        transition: { duration : 1 }
-    }
-}
+const path = {
+        transition: {
+        duration: 4, //Change this to either slow or hasten the animation
+        ease: "easeInOut",
+    },
+    variants: {
+        visible: {
+            pathLength: 1, //Change this to determine how much of the path is drawn when first rendered
+    },
+    hidden: {
+        pathLength: 0,
+        },
+    },
+};
+
+const pathColor = "black";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const vb = `0 0 ${windowWidth} ${windowHeight}`
 
 function Home() {
     return (
@@ -84,7 +98,22 @@ function Home() {
                         uri: 'https://s3-us-west-1.amazonaws.com/co-directory-images/afm-rahman-84141221.jpg',
                         }}
                     /> 
-                    <Text style = {styles.name}> ZAYAAN RAHMAN </Text>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="800" height="600" viewBox={vb}>
+                <g transform="translate(500 100)">
+                    <motion.path
+                        {...path}
+                        animate="visible"
+                        initial="hidden"
+                        //d="M154.159,159.064c14.534,0,29.069.063,43.6-.046a12.556,12.556,0,0,1,8.923,2.922c9.926,8.437,19.947,16.771,30.149,24.871,3.719,2.953,3.852,4.888.247,8.109q-39.8,35.564-79.3,71.45c-2.775,2.517-4.694,2.734-7.563.133q-39.827-36.089-79.879-71.929c-3.5-3.129-2.766-5.007.459-7.6q15.512-12.493,30.711-25.372c2.788-2.375,5.741-2.609,9.048-2.6,14.534.054,29.069.024,43.6.024Z"
+                        d="M 400 300 L 550 300 L 400 450 L 550 450 "
+                        fill="none"
+                        stroke={pathColor}
+                        //stroke-miterlimit="10"
+                        strokeWidth="3"
+                    />
+                </g>
+            </svg>
+                    {/* <Text style = {styles.name}> ZAYAAN RAHMAN </Text> */ }
                     <View style={{paddingVertical: 10}}></View>
                     <Text style = {styles.description}>I am a Computer Science student studying at SUNY University at Buffalo currently in my third year.</Text>
                     <Text style = {styles.description}>Currently, I am interested in cybersecurity, web development, and how important those are in the modern world.</Text>
