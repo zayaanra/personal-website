3429644711235b287d0aaad52ce3e181bec971d1
Zayaan <zayaanra@buffalo.edu>
2022-01-02 17:18:22 -0500

    Added a simple anim. for the links box

--- frontend/app/src/components/Links.js
+++ frontend/app/src/components/Links.js
@@ -31,23 +31,25 @@ const styles = StyleSheet.create({
 
 function Links() {
     return(
-        <View style={styles.linksBox}> 
-            <Text style={styles.linksTitle}>Contact</Text>
-            <Text style={styles.linksText}>Mobile: 631-839-0886 | E-mail: rahmanzayaan43@gmail.com</Text>
-            <Text style={styles.linksTitle}>Links</Text>
-            <View style={styles.linksImage}>
-                <motion.button style={{backgroundColor: "white"}} whileHover={{ scale: 1.5 }}>
-                <TouchableOpacity onPress={() => {Linking.openURL("https://www.linkedin.com/in/zayaan-rahman-7b6368213/")}}>
-                    <Image animation="pulse" style={{width: 20, height: 20}} source={require("../images/linkedin.png")}></Image>
-                </TouchableOpacity>
-                </motion.button>
-                <motion.button style={{backgroundColor: "white"}} whileHover={{ scale: 1.5 }}>
-                <TouchableOpacity onPress={() => {Linking.openURL("https://github.com/zayaanra")}}>
-                    <Image style={{width: 20, height: 20}} source={require("../images/GitHub-Mark.png")}></Image>
-                </TouchableOpacity>
-                </motion.button> 
+        <motion.div animate={{x: [-100, 100]}} transition={{ yoyo: Infinity, duration: 3, repeat: Infinity}}>
+            <View style={styles.linksBox}> 
+                <Text style={styles.linksTitle}>Contact</Text>
+                <Text style={styles.linksText}>Mobile: 631-839-0886 | E-mail: rahmanzayaan43@gmail.com</Text>
+                <Text style={styles.linksTitle}>Links</Text>
+                <View style={styles.linksImage}>
+                    <motion.button style={{backgroundColor: "white"}} whileHover={{ scale: 1.5 }}>
+                    <TouchableOpacity onPress={() => {Linking.openURL("https://www.linkedin.com/in/zayaan-rahman-7b6368213/")}}>
+                        <Image animation="pulse" style={{width: 20, height: 20}} source={require("../images/linkedin.png")}></Image>
+                    </TouchableOpacity>
+                    </motion.button>
+                    <motion.button style={{backgroundColor: "white"}} whileHover={{ scale: 1.5 }}>
+                    <TouchableOpacity onPress={() => {Linking.openURL("https://github.com/zayaanra")}}>
+                        <Image style={{width: 20, height: 20}} source={require("../images/GitHub-Mark.png")}></Image>
+                    </TouchableOpacity>
+                    </motion.button> 
+                </View>
             </View>
-        </View>
+        </motion.div>
     )
 }
 
