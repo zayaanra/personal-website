5c373d4a165dc8ef210f45202addbf864209abc4
Zayaan <zayaanra@buffalo.edu>
2022-01-05 17:54:37 -0500

    Added temp. html

--- frontend/app/src/components/Skills.js
+++ frontend/app/src/components/Skills.js
@@ -1,12 +1,15 @@
 /* This file will support a page that lists and describes the skills that I have */
 
+import { View } from "react-native";
 import React from "react";
+
+import TopBar from "./TopBar";
 
 function Skills() {
     return(
-        <div>
-            Skills page
-        </div>
+        <View>
+            <TopBar></TopBar>
+        </View>
     )
 }
 
