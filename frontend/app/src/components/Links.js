a11ef5b2559e9f1739c0aa0bb60c93b9f5ac146e
Zayaan <zayaanra@buffalo.edu>
2022-01-05 17:54:04 -0500

    Removed yoyo

--- frontend/app/src/components/Links.js
+++ frontend/app/src/components/Links.js
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
         width: 1000,
         height: 100,
         borderWidth: 1,
+        borderRadius: 100,
         borderColor: "black",
         alignItems: "center"
     },
