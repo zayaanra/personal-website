f6f82d49c60d52396ee81e3e7d1b683a14a6ad63
Zayaan <zayaanra@buffalo.edu>
2022-01-12 19:59:10 -0500

    Added basic template for projects tab

--- frontend/app/src/components/Projects.js
+++ frontend/app/src/components/Projects.js
@@ -1,12 +1,31 @@
 /* This file will support a page that lists and describes the projects I have written */
 
+import { StyleSheet, View, Image, Text, } from "react-native";
 import React from "react";
+
+import TopBar from "./TopBar";
+import FadeIn from "./FadeIn";
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    name: {
+        fontFamily: "Iceland",
+        fontSize: 40,
+        textDecorationLine: "underline",
+    }
+})
 
 function Projects() {
     return(
-        <div>
-            Projects page
-        </div>
+        <View style={styles.container}>
+            <TopBar></TopBar>
+            <FadeIn>
+                <Text style={styles.name}>BULLBOARD</Text>
+            </FadeIn>
+        </View>
     )
 }
 
