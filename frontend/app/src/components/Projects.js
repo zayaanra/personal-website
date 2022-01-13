48eb560dc08777e15ac66fee06c5da2c7198c9bc
Zayaan <zayaanra@buffalo.edu>
2022-01-13 16:18:35 -0500

    Updated

--- frontend/app/src/components/Projects.js
+++ frontend/app/src/components/Projects.js
@@ -1,6 +1,6 @@
 /* This file will support a page that lists and describes the projects I have written */
 
-import { StyleSheet, View, Image, Text, } from "react-native";
+import { StyleSheet, View, Image, Text, Linking, TouchableOpacity } from "react-native";
 import React from "react";
 
 import TopBar from "./TopBar";
@@ -21,6 +21,11 @@ const styles = StyleSheet.create({
     desc: {
         fontFamily: "Arial",
         fontSize: 20,
+    },
+    link: {
+        fontFamily: "Arial",
+        fontSize: 20,
+        textDecorationLine: "underline"
     }
 })
 
@@ -37,6 +42,9 @@ function Projects() {
                 Users can also make posts and interact on a live map with other users.</Text>
                 <Text style={styles.desc}>Python was used as a back-end along with the aiohttp library and pure HTML/CSS/JavaScript was used as the front-end.</Text>
                 <Text style={styles.desc}>MongoDB was used as a database to store account information.</Text>
+                <TouchableOpacity onPress={() => {Linking.openURL("https://github.com/ethan-richardson/Bullboard")}}>
+                    <Text style={styles.link}>https://github.com/ethan-richardson/Bullboard</Text>
+                </TouchableOpacity>
             </FadeIn>
             <View style={{padding: 20}}/>
             <FadeIn>
@@ -46,6 +54,9 @@ function Projects() {
                 <Text style={styles.desc}>Specifically, I learned what React is, the differences between React and React-Native/Expo.</Text>
                 <Text style={styles.desc}>In terms of building the website, I learned the importance of React hooks, components, and the various open source libraries provided for React-Native.</Text>
                 <Text style={styles.desc}>I also wish to improve my knowledge of React/React-Native in the future and improve upon this website.</Text>
+                <TouchableOpacity onPress={() => {Linking.openURL("https://github.com/zayaanra/personal-website")}}>
+                    <Text style={styles.link}>https://github.com/zayaanra/personal-website</Text>
+                </TouchableOpacity>
             </FadeIn>
             <View style={{padding: 20}}/>
             <FadeIn>
@@ -55,6 +66,7 @@ function Projects() {
                 <Text style={styles.desc}>This allocator replaced standard C allocation functions such as as malloc, calloc, and realloc.</Text>
                 <Text style={styles.desc}>Things I learned include how the stack and heap work and how memory is allocated.</Text>
                 <Text style={styles.desc}>After completion of the project, the allocator could be used to run UNIX commands such as ls, ps aux, and large scale applications such as VIM.</Text>
+                <Text style={styles.desc}>Due to UB's academic integrity policy, I am unable to provide the repository publicly. However, it is available upon request.</Text>
             </FadeIn>
         </View>
     )
